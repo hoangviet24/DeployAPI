b59222104d162612594adcca728e6011bf29cb4c
hoangviet24 <hoangvietdoican2004@gmail.com>
2025-04-17 10:39:49 +0700

    update

--- src/main/java/product/management/electronic/constants/AppConstant.java
+++ src/main/java/product/management/electronic/constants/AppConstant.java
@@ -2,7 +2,7 @@ package product.management.electronic.constants;
 
 public class AppConstant {
     public static final String[] WHITE_LIST_URL = {
-            "/",
+            "https://deployapi-xw5c.onrender.com",
             "/api/v1/auth/**", "/v2/api-docs", "/v3/api-docs",
             "/v3/api-docs/**", "/swagger-resources", "/swagger-resources/**", "/configuration/ui",
             "/configuration/security", "/swagger-ui/**", "/webjars/**", "/swagger-ui.html", "/api/auth/**",
