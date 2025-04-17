c503f22148fbe8dad107ae5ecaf2d4bae985a40d
hoangviet24 <hoangvietdoican2004@gmail.com>
2025-04-17 10:33:47 +0700

    update white list

--- src/main/java/product/management/electronic/constants/AppConstant.java
+++ src/main/java/product/management/electronic/constants/AppConstant.java
@@ -2,6 +2,7 @@ package product.management.electronic.constants;
 
 public class AppConstant {
     public static final String[] WHITE_LIST_URL = {
+            "/",
             "/api/v1/auth/**", "/v2/api-docs", "/v3/api-docs",
             "/v3/api-docs/**", "/swagger-resources", "/swagger-resources/**", "/configuration/ui",
             "/configuration/security", "/swagger-ui/**", "/webjars/**", "/swagger-ui.html", "/api/auth/**",
